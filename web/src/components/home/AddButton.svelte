10fc01b3e322945da2ec6930ec7940c3f0e2b552
Aaron Mamparo <aaronmamparo@gmail.com>
2023-05-02 23:33:06 -0500

    resize

--- web/src/components/home/AddButton.svelte
+++ web/src/components/home/AddButton.svelte
@@ -2,8 +2,8 @@
   import { push } from "svelte-spa-router";
 </script>
 
-<button class="rounded-lg text-green-400 p-10 border-green-400 border-4 w-fill
+<button class="rounded-lg text-green-400 p-5 border-green-400 border-4 w-fill
 col-span-12 md:col-start-5 md:col-end-9 xl:col-start-6 xl:col-end-8"
         on:click={() => push('/add')}>
-  Add Song
+  Add
 </button>
